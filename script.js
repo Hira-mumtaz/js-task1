6d630ac1ad602fb01386bdec4029abfea4aefa6d
Hira-mumtaz <h.mumtaz@pk.micromerger.com>
2025-12-01 14:55:48 +0500

    js task

---
+++ script.js
@@ -0,0 +1,11 @@
+const accountId = 230
+let accountEmail = "hiramalik@gmail.com"
+var accountPassword = "123"
+accountcity="talagang"
+// accountId = 260
+accountEmail = "hiramalik677@gmail.com"
+accountPassword = "12344"
+accountcity="Rawalpindi"
+
+console.log(accountId);
+console.table([accountcity, accountPassword, accountEmail])
