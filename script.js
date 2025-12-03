bff8627abfe9c1b1c107a4f496a977a5b5fbdfb0
Hira-mumtaz <h.mumtaz@pk.micromerger.com>
2025-12-03 16:58:05 +0500

    use Dom

--- script.js
+++ script.js
@@ -9,3 +9,54 @@ accountcity="Rawalpindi"
 
 console.log(accountId);
 console.table([accountcity, accountPassword, accountEmail])
+
+function addTwoNumber (number1,  number2){
+    console .log (number1+ number2);
+
+}
+addTwoNumber(3,5)
+
+function multiplyTwoNumber(number1 ,  number2){
+    let result = number1 * number2
+    return result;
+   
+}
+
+const result = multiplyTwoNumber(3, 5)
+console.log(result)
+
+function multiplyTwoNumber(number1 ,  number2){
+    
+    return number1 * number2;
+   
+}
+
+const result2 = multiplyTwoNumber(3, 5)
+console.log(result)
+
+function loginusermessage(username){
+   return `${username} just logged in`
+
+}
+console.log (loginusermessage("hira"))
+
+
+const add = (a, b) => a+b;
+
+console.log(add(3,4))
+
+const sum =(a,b) => a-b;
+console.log(sum(6, 5))
+
+const multi=(b,c) => b*c;
+console.log(multi(5, 5))
+
+let box = document.getElementsByClassName('box1')[0]
+let h1 = document.getElementsByTagName('h1')[0]
+
+h1.addEventListener("click" , function(){
+    box.style.backgroundColor='red'
+
+});
+
+
